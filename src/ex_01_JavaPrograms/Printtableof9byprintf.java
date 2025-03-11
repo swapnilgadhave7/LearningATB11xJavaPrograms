630ebfff7dfea21d68a38fce660c79e0fb7e7e51
SWAPNIL <swapnilsg99@gmail.com>
2025-03-11 23:00:59 +0530

    11-3-2025  code commit today.

--- src/ex_01_JavaPrograms/Printtableof9byprintf.java
+++ src/ex_01_JavaPrograms/Printtableof9byprintf.java
@@ -2,9 +2,7 @@ package ex_01_JavaPrograms;
 
 public class Printtableof9byprintf {
     public static void main(String[] args) {
-
         int a=9;
-
         for(int i=1;i<=10;i++)
         {
             int mul=a*i;
