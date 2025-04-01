2869ff56f00a45aad82db0696f36304b2b1515bd
SWAPNIL <swapnilsg99@gmail.com>
2025-04-01 21:58:18 +0530

    01-04-2025  code commit today.

---
+++ src/Tasks/Class_Person.java
@@ -0,0 +1,51 @@
+package Tasks;
+
+public class Class_Person {
+    public static void main(String[] args) {
+        Person ref1= new Person();
+        ref1.setname("Tara");
+        ref1.eat();
+        ref1.speak("English");
+        ref1.hobby();
+        double weightpresent =ref1.getWeight(23.23);
+        System.out.println(ref1.name+" "+weightpresent);
+        String hobbyofperson = ref1.hobby();
+        System.out.println(ref1.name+" "+hobbyofperson);
+    }
+}
+class Person
+{
+    String name;
+    String age;
+    double weight;
+    String eat;
+    String read;
+    String gender;
+    String hobby;
+    String speak;
+    int walkdaily;
+    String readbook;
+    void setname(String name)
+    {
+        this.name=name;
+    }
+    void eat()
+    {
+        System.out.println("Person eating daily 3 times");
+
+    }
+    void speak(String speak)
+    {
+        this.speak=speak;
+    }
+    double getWeight(double weight)
+    {
+        this.weight=weight;
+        return weight;
+    }
+    String hobby()
+    {
+        
+        return "painting";
+    }
+}
